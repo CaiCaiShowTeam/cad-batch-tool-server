a79c1b56b15eb6489269169ecfeb13b84872f750
Jeason <Jeason@172.20.10.3>
2018-10-05 13:51:33 +0800

    add dependency.Jeason

---
+++ src/com/bplead/cad/util/ServerUtils.java
@@ -0,0 +1,11 @@
+package com.bplead.cad.util;
+
+import java.io.Serializable;
+
+import wt.method.RemoteAccess;
+
+public class ServerUtils implements RemoteAccess, Serializable {
+
+	private static final long serialVersionUID = -1621878372079381524L;
+
+}
